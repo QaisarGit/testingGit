6fa31f16c357ad191f2ad93269de41dcd069494a
QaisarGit <qaisar.fareed@gmail.com>
2026-03-04 16:09:11 +0530

    commit 800

--- src/test/java/PROJECTS/ReverseStringNumbers.java
+++ src/test/java/PROJECTS/ReverseStringNumbers.java
@@ -4,26 +4,41 @@ import org.testng.annotations.Test;
 
 public class ReverseStringNumbers {
 
-	//@Test(description="using string builder")
+	// @Test(description="using string builder")
 	public void reversedSTRING() {
 		System.out.println("reversed word ");
-		String str= "I write a program that match the requirements";
-		StringBuilder sb= new StringBuilder(str);
+		String str = "I write a program that match the requirements";
+		StringBuilder sb = new StringBuilder(str);
 		sb.reverse();
-		System.out.println("reversed str is ==\n"+sb);
-		
-	}
-	@Test(description="using for loop")
-	public void reversedStringLoop() {
-		System.out.println("using  for loop ");
-		String str= "I write a program that match the requirements";
-		String rev ="";
-		for(int i =str.length()-1;i>=0;i--) {
-			rev=rev+str.charAt(i);
-			
-			
-		}
-		System.out.println(rev); 
+		System.out.println("reversed str is ==\n" + sb);
+
 	}
 
+	// @Test(description = "using for loop")
+	public void reversedStringLoop() {
+		System.out.println("using  for loop ");
+		String str = "I write a program that match the requirements";
+		String rev = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			rev = rev + str.charAt(i);
+
+		}
+		System.out.println(rev);
+	}
+
+	@Test(description = "reversed  order words in a sentance")
+	public void reveredWordOrder() {
+		String str = "I wrote many books and articles";
+
+		String[] words = str.split(" ");
+
+		for (int i = words.length - 1; i >= 0; i--) {
+			System.out.print(words[i] + " ");
+		}
+
+	}
+	@Test(description="reversed word")
+public void reversedWords() {
+		
+	}
 }
