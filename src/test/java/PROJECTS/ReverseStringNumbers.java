988653cd5585ac417d14ba30252250e3a94dab0d
QaisarGit <qaisar.fareed@gmail.com>
2026-03-04 15:40:04 +0530

    reversed strings

--- src/test/java/PROJECTS/ReverseStringNumbers.java
+++ src/test/java/PROJECTS/ReverseStringNumbers.java
@@ -4,16 +4,18 @@ import org.testng.annotations.Test;
 
 public class ReverseStringNumbers {
 
-	@Test
-	public void LargestSmallestSTRING() {
-		System.out.println("Largest word ");
-	
-		
-		System.out.println("Largest word ");
-		System.out.println("Largest word ");
-		System.out.println("Largest word ");
-		System.out.println("Largest word ");
-		
+	@Test(description="using string builder")
+	public void reversedSTRING() {
+		System.out.println("reversed word ");
+		String str= "I write a program that match the requirements";
+		StringBuilder sb= new StringBuilder(str);
+		sb.reverse();
+		System.out.println("reversed str is ==\n"+sb);
 		
 	}
+	@Test(description="using for loop")
+	public void reversedStringLoop() {
+		System.out.println("using  for loop ");
+	}
+
 }
